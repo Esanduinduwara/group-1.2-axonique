c5116eced93c43992e43cb745c090d8123e2a71d
Mitheja Manoharie <mmanoharie@gmail.com>
2026-04-01 23:43:36 +0530

    Remove security question fields from users table in database schema

--- init.sql
+++ init.sql
@@ -6,12 +6,6 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    security_question_1 VARCHAR(255),
-    security_answer_1 VARCHAR(255),
-    security_question_2 VARCHAR(255),
-    security_answer_2 VARCHAR(255),
-    security_question_3 VARCHAR(255),
-    security_answer_3 VARCHAR(255),
     enabled BOOLEAN DEFAULT FALSE,
     role VARCHAR(20) NOT NULL DEFAULT 'CUSTOMER'
 );
